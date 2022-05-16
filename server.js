50af5f51f956d32ae5de01c4d3ab832f7404b331
Tony Tran <tonytran504@gmail.com>
2022-05-16 16:34:17 -0500

    added JOIN

--- server.js
+++ server.js
@@ -29,7 +29,12 @@ const db = mysql.createConnection(
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id 
+    WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
@@ -103,7 +108,11 @@ const params = [1, 'Ronald', 'Firbank', 1];
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
